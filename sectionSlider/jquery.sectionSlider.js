023101f3017233397dcbbfa8327b6886a7f6102f
Олег <olegblud@gmail.com>
2015-08-19 15:03:36 +0300

    Plugins

---
+++ sectionSlider/jquery.sectionSlider.js
@@ -0,0 +1,139 @@
+(function ($) {
+	$.fn.sectionSlider = function (User_Settings) {
+
+		// Set Settings
+		var Input = this;
+		var Default_Settings = {
+			'Height':   'window', // window, auto
+			'Effect':   'fade', // none, fade, slide, vertical_slide,corner_slide
+			'Duration': 444
+		};
+		var Settings = $.extend(Default_Settings, User_Settings);
+
+		// Set Height
+		function SetSectionHeight() {
+			$(Input).css('height', $('.Section-Slider-Item.Active', Input).actual('outerHeight') + 'px');
+		}
+
+		// Set Window
+		function SetWindowHeight() {
+			$(Input).css('height', $(window).height() + 'px');
+		}
+
+		if (Settings.Height == 'auto') {
+			SetSectionHeight();
+			$(window).resize(function () {
+				SetSectionHeight();
+			});
+		} else if (Settings.Height == 'window') {
+			SetWindowHeight();
+			$(window).resize(function () {
+				SetWindowHeight();
+			});
+		}
+
+		// Activate
+		$('.Section-Slider-Item', Input).css('display', 'none');
+		$('.Section-Slider-Item.Active', Input).css('display', 'block');
+
+		// Change Slide
+		var Count_Slides = $('.Section-Slider-Item', Input).length;
+
+
+		// Next Slide
+		$('.Section-Slider-Next', Input).click(function () {
+			var Current_Slide = $('.Section-Slider-Item.Active', Input).index();
+			if (Current_Slide < Count_Slides - 1) {
+				Current_Slide++;
+			} else {
+				Current_Slide = 0;
+			}
+			Slide_Effect(Current_Slide, 'right');
+		});
+
+
+		// Prev Slide
+		$('.Section-Slider-Previous', Input).click(function () {
+			var Current_Slide = $('.Section-Slider-Item.Active', Input).index();
+			if (Current_Slide == 0) {
+				Current_Slide = Count_Slides - 1;
+			} else {
+				Current_Slide--;
+			}
+			Slide_Effect(Current_Slide, 'left');
+		});
+
+		// Button Slide
+		$('.Section-Slider-Button', Input).click(function () {
+			var Current_Slide = $(this).index();
+			Slide_Effect(Current_Slide);
+		});
+
+
+		// Slide Effect
+		function Slide_Effect(Current_Slide, Direction) {
+			var Items = $('.Section-Slider-Item', Input);
+			var Buttons = $('.Section-Slider-Button', Input);
+
+			// Change Active
+			$(Buttons.removeClass('Active')[Current_Slide]).addClass('Active');
+
+
+			$(Items.removeClass('Active')[Current_Slide]).addClass('Active');
+
+			Items.removeClass('Active');
+			$(Items.removeClass('Active')[Current_Slide]).addClass('Active');
+
+			// Effect
+			if (Settings.Effect == 'fade') {
+				Items.fadeOut(Settings.Duration);
+				$(Items[Current_Slide]).fadeIn(Settings.Duration);
+			} else if (Settings.Effect == 'slide') {
+				if (Direction == 'right') {
+					Items.not(Items[Current_Slide]).animate({'left': $(window).width() + 'px'}, Settings.Duration);
+					$(Items[Current_Slide]).css({
+						'left': '-' + $(window).width() + 'px', 'display': 'block'
+					}).animate({'left': 0}, Settings.Duration);
+				} else {
+					Items.not(Items[Current_Slide]).animate({
+						'left': '-' + $(window).width() + 'px'
+					}, Settings.Duration);
+					$(Items[Current_Slide]).css({
+						'left': $(window).width() + 'px', 'display': 'block'
+					}).animate({'left': 0}, Settings.Duration);
+				}
+			} else if (Settings.Effect == 'corner_slide') {
+
+				if (Direction == 'right') {
+					Items.not(Items[Current_Slide]).animate({
+						'left': $(window).width() + 'px', 'top': $(window).height()
+					}, Settings.Duration);
+					$(Items[Current_Slide]).css({
+						'left':    '-' + $(window).width() + 'px', 'top': '-' + $(window).height() + 'px',
+						'display': 'block'
+					}).animate({'left': 0, 'top': '0'}, Settings.Duration);
+				} else {
+					Items.not(Items[Current_Slide]).animate({
+						'left': '-' + $(window).width() + 'px', 'top': '-' + $(window).height() + 'px'
+					}, Settings.Duration);
+					$(Items[Current_Slide]).css({
+						'left': $(window).width() + 'px', 'top': $(window).height() + 'px', 'display': 'block'
+					}).animate({'left': 0, 'top': '0'}, Settings.Duration);
+				}
+
+			} else if (Settings.Effect == 'vertical_slide') {
+				Items.not(Items[Current_Slide]).slideUp(Settings.Duration);
+				$(Items[Current_Slide]).slideDown(Settings.Duration);
+			} else {
+				Items.css('display', 'none');
+				$(Items[Current_Slide]).css('display', 'block');
+			}
+
+		}
+
+
+	}
+})(jQuery);
+
+
+
